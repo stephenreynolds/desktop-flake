dd3b5ad25f958388a2735e4a64e4f48ec4727473
Stephen Reynolds <mail@stephenreynolds.dev>
2024-02-16 16:49:44 -0500

    feat(theme): restore wallpaper and color theme on startup

--- ags/js/settings/setup.js
+++ ags/js/settings/setup.js
@@ -1,8 +1,17 @@
 import { setupHyprland } from './hyprland.js';
 import { reloadScss, scssWatcher } from './scss.js';
+import * as Utils from 'resource:///com/github/Aylur/ags/utils.js';
+
+async function restoreTheme() {
+    await Utils.execAsync([
+        `${App.configDir}/scripts/color_generation/switchwall.sh`,
+        `${Utils.CACHE_DIR}/user/wallpaper`
+    ]);
+}
 
 export function init() {
     scssWatcher();
     reloadScss();
     setupHyprland();
+    restoreTheme();
 }
