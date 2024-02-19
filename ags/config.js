28ad774745170996dff34fbcfce40c4b097ee038
Stephen Reynolds <mail@stephenreynolds.dev>
2024-02-19 17:53:53 -0500

    formatting

--- ags/config.js
+++ ags/config.js
@@ -1,18 +1,17 @@
-const main = "/tmp/ags/main.js";
+const main = '/tmp/ags/main.js';
 const outdir = `${App.configDir}/ts/main.ts`;
 
 try {
     await Utils.execAsync([
-        "bun", "build", outdir,
-        "--outfile", main,
-        "--external", "resource://*",
-        "--external", "gi://*",
-        "--external", "file://*",
+        'bun', 'build', outdir,
+        '--outfile', main,
+        '--external', 'resource://*',
+        '--external', 'gi://*',
+        '--external', 'file://*',
     ]);
 } catch (error) {
     console.error(error);
     App.quit();
 }
 
-const { default: config } = await import(`file://${main}`);
-export default config;
+export default (await import(`file://${main}`)).default;
