5b90ec98bc9b30acedc76d6b4d57ca4ec82fdbe9
Stephen Reynolds <mail@stephenreynolds.dev>
2024-02-25 23:44:15 -0500

    css(bar): reduce font size of clock

--- ags/ts/widgets/bar/Clock.ts
+++ ags/ts/widgets/bar/Clock.ts
@@ -3,7 +3,7 @@ import Clock from 'widgets/misc/Clock';
 import options from 'options';
 
 export default () => Widget.Box({
-    className: 'text text-sm',
+    className: 'text text-xs',
     vertical: true,
     vpack: 'center',
     children: [
