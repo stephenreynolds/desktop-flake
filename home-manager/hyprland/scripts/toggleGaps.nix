964c2101075045b1c5590e64efcd4e454f2d1b58
Stephen Reynolds <mail@stephenreynolds.dev>
2024-03-24 14:06:07 -0400

    hyprland: toggle rounding when toggling gaps

--- home-manager/hyprland/scripts/toggleGaps.nix
+++ home-manager/hyprland/scripts/toggleGaps.nix
@@ -6,9 +6,11 @@ in pkgs.writeShellScript "toggle_hyprland_gaps" ''
 
   if [[ $gaps_out -eq 0 ]]; then
     gaps_out=10
+    rounding=4
     state="enabled"
   else
     gaps_out=0
+    rounding=0
     state="disabled"
   fi
 
@@ -18,7 +20,7 @@ in pkgs.writeShellScript "toggle_hyprland_gaps" ''
     gaps_in=0
   fi
 
-  ${hyprctl} --batch "keyword general:gaps_out $gaps_out ; keyword general:gaps_in $gaps_in" 
+  ${hyprctl} --batch "keyword general:gaps_out $gaps_out ; keyword general:gaps_in $gaps_in ; keyword general:rounding $rounding" 
 
   ${pkgs.libnotify}/bin/notify-send "Hyprland" "Gaps $state" --transient
 ''
