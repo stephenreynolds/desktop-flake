596f63fc869f395d280802a2376744d12d22ff81
Stephen Reynolds <mail@stephenreynolds.dev>
2024-04-07 00:19:33 -0400

    feat(autostart): add delay before playing login sound

--- home-manager/hyprland/autostart.nix
+++ home-manager/hyprland/autostart.nix
@@ -4,26 +4,32 @@ let
   inherit (lib) mkIf getExe optionalString optionals;
   cfg = config.desktop-flake.hyprland;
   primaryMonitor = config.desktop-flake.primaryMonitor;
-in mkIf cfg.enable {
+in
+mkIf cfg.enable {
   wayland.windowManager.hyprland.settings = {
     exec = [ "${getExe pkgs.xorg.xrandr} --output ${primaryMonitor} --primary" ]
-      ++ (optionals config.desktop-flake.ags.enable (let
+      ++ (optionals config.desktop-flake.ags.enable (
+      let
         ags =
           "${inputs.ags.packages.${pkgs.system}.default}/bin/ags -b hyprland";
-      in [ "${ags} -q; ${ags}" ]));
+      in
+      [ "${ags} -q; ${ags}" ]
+    ));
 
-    exec-once = let
-      hyprctl = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl";
-      cliphist = "${pkgs.cliphist}/bin/cliphist";
-      wl-paste = "${pkgs.wl-clipboard}/bin/wl-paste";
-    in [
-      "${pkgs.libcanberra-gtk3}/bin/canberra-gtk-play -f ${pkgs.yaru-theme}/share/sounds/Yaru/stereo/desktop-login.oga"
+    exec-once =
+      let
+        hyprctl = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl";
+        cliphist = "${pkgs.cliphist}/bin/cliphist";
+        wl-paste = "${pkgs.wl-clipboard}/bin/wl-paste";
+      in
+      [
+        "sleep 0.5 ; ${pkgs.libcanberra-gtk3}/bin/canberra-gtk-play -f ${pkgs.yaru-theme}/share/sounds/Yaru/stereo/desktop-login.oga"
 
-      (optionalString (primaryMonitor != "")
-        "${hyprctl} dispatch focusmonitor ${primaryMonitor}")
+        (optionalString (primaryMonitor != "")
+          "${hyprctl} dispatch focusmonitor ${primaryMonitor}")
 
-      "${wl-paste} --type text --watch ${cliphist} store"
-      "${wl-paste} --type image --watch ${cliphist} store"
-    ];
+        "${wl-paste} --type text --watch ${cliphist} store"
+        "${wl-paste} --type image --watch ${cliphist} store"
+      ];
   };
 }
