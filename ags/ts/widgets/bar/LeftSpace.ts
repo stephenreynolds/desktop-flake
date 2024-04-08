fc8e68d14e05e513c77ba91c0eb7147d968e3b1a
Stephen Reynolds <mail@stephenreynolds.dev>
2024-04-07 23:06:18 -0400

    refactor(bar): refactor LeftSpace

--- ags/ts/widgets/bar/LeftSpace.ts
+++ ags/ts/widgets/bar/LeftSpace.ts
@@ -1,61 +1,60 @@
 import Widget from 'resource:///com/github/Aylur/ags/widget.js';
-import Hyprland from 'resource:///com/github/Aylur/ags/service/hyprland.js';
 import { type Workspace } from 'types/service/hyprland';
 
-export default (monitor: number) => {
-    const dispatch = (arg) => Hyprland.messageAsync(`dispatch workspace ${arg}`)
+const hyprland = await Service.import('hyprland');
 
-    const getMonitorWorkspaces = (monitor: number) =>
-        Hyprland.workspaces
-            .filter((w) =>
-                w.monitor === Hyprland.getMonitor(monitor).name &&
-                w.id !== -99)
-            .sort((a, b) => a.id - b.id);
+const dispatchWorkspace = (arg: string) => hyprland.messageAsync(`dispatch workspace ${arg}`)
 
-    const workspaceButton = (workspace: Workspace, monitor: number) => Widget.Button({
-        onClicked: () => Hyprland.messageAsync(`dispatch workspace ${workspace.id}`),
-        child: Widget.Label({
-            label: `${workspace.name}`,
-            className: 'indicator',
-            vpack: 'center',
-        }),
-        setup: (self) => self.hook(Hyprland, () => {
-            try {
-                self.toggleClassName('active', Hyprland.getMonitor(monitor).activeWorkspace.id === workspace.id);
-                self.toggleClassName('focused', Hyprland.active.workspace.id === workspace.id);
-            }
-            catch {
+const getMonitorWorkspaces = (monitor: number) =>
+    hyprland.workspaces
+        .filter((w) =>
+            w.monitor === hyprland.getMonitor(monitor)?.name &&
+            w.id !== -99)
+        .sort((a, b) => a.id - b.id);
+
+const workspaceButton = (workspace: Workspace, monitor: number) => Widget.Button({
+    onClicked: () => hyprland.messageAsync(`dispatch workspace ${workspace.id}`),
+    child: Widget.Label({
+        label: `${workspace.name}`,
+        className: 'indicator',
+        vpack: 'center',
+    }),
+    setup: (self) => self.hook(hyprland, () => {
+        try {
+            self.toggleClassName('active', hyprland.getMonitor(monitor)?.activeWorkspace.id === workspace.id);
+            self.toggleClassName('focused', hyprland.active.workspace.id === workspace.id);
+        }
+        catch {
+            return;
+        }
+    }),
+});
+
+const workspaces = (monitor: number) => Widget.EventBox({
+    className: 'workspaces',
+    onScrollUp: () => dispatchWorkspace('m+1'),
+    onScrollDown: () => dispatchWorkspace('m-1'),
+    child: Widget.Box({
+        vpack: 'center',
+        setup: (self) => self.hook(hyprland, (self, event, name) => {
+            if (self.children.length === 0) {
+                self.children = getMonitorWorkspaces(monitor)
+                    .map((w) => workspaceButton(w, monitor));
                 return;
             }
-        }),
-    });
 
-    const workspaces = Widget.EventBox({
-        className: 'workspaces',
-        onScrollUp: () => dispatch('m+1'),
-        onScrollDown: () => dispatch('m-1'),
-        child: Widget.Box({
-            vpack: 'center',
-            setup: (self) => self.hook(Hyprland, (self, event, name) => {
-                if (self.children.length === 0) {
-                    self.children = getMonitorWorkspaces(monitor)
-                        .map((w) => workspaceButton(w, monitor));
-                    return;
-                }
+            if ((event === 'createworkspace' || event === 'destroyworkspace') && name !== 'special') {
+                self.children = getMonitorWorkspaces(monitor)
+                    .map((w) => workspaceButton(w, monitor));
+            }
+        }, 'event'),
+    }),
+});
 
-                if ((event === 'createworkspace' || event === 'destroyworkspace') && name !== 'special') {
-                    self.children = getMonitorWorkspaces(monitor)
-                        .map((w) => workspaceButton(w, monitor));
-                }
-            }, 'event'),
-        }),
-    });
-
-    return Widget.Box({
-        className: 'bar-space',
-        hpack: 'start',
-        children: [
-            workspaces,
-        ],
-    });
-};
+export default (monitor: number) => Widget.Box({
+    className: 'bar-space',
+    hpack: 'start',
+    children: [
+        workspaces(monitor),
+    ],
+});
