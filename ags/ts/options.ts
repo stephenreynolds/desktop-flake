4ae2a9d8573b41abccd974c71659c078de7caf7c
Stephen Reynolds <mail@stephenreynolds.dev>
2024-04-22 19:18:55 -0400

    option: change primary monitor

--- ags/ts/options.ts
+++ ags/ts/options.ts
@@ -1,36 +1,42 @@
-import { Option, resetOptions, getValues, apply, getOptions } from './settings/option.js';
+import {
+  Option,
+  resetOptions,
+  getValues,
+  apply,
+  getOptions,
+} from "./settings/option.js";
 
 export default {
-    reset: resetOptions,
-    values: getValues,
-    apply: apply,
-    list: getOptions,
+  reset: resetOptions,
+  values: getValues,
+  apply: apply,
+  list: getOptions,
 
-    bar: {
-        position: Option('bottom', {
-            'enum': ['top', 'bottom'],
-            'type': 'enum',
-        }),
-        showOnAllMonitors: Option(true),
+  bar: {
+    position: Option("bottom", {
+      enum: ["top", "bottom"],
+      type: "enum",
+    }),
+    showOnAllMonitors: Option(true),
+  },
+
+  primaryMonitor: Option(2),
+
+  hyprland: {
+    borders: {
+      size: Option(1),
     },
-
-    primaryMonitor: Option(1),
-
-    hyprland: {
-        borders: {
-            size: Option(1),
-        },
-        gaps: {
-            gapsIn: Option(0),
-            gapsOut: Option(0),
-            noGapsWhenOnly: Option(0),
-            noGapsWindowClasses: Option(['firefox']),
-        },
-        rounding: Option(0),
+    gaps: {
+      gapsIn: Option(0),
+      gapsOut: Option(0),
+      noGapsWhenOnly: Option(0),
+      noGapsWindowClasses: Option(["firefox"]),
     },
+    rounding: Option(0),
+  },
 
-    locale: {
-        dateFormat: Option('%a %b %-e'),
-        timeFormat: Option('%-I:%M %p'),
-    }
+  locale: {
+    dateFormat: Option("%a %b %-e"),
+    timeFormat: Option("%-I:%M %p"),
+  },
 };
