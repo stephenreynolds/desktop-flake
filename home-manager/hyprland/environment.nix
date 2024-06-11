7fc8b6db248c255cffacc3c6eb3ae9db189ae5ed
Stephen Reynolds <mail@stephenreynolds.dev>
2024-06-10 22:27:22 -0400

    refactor(hyprland): enable hardware cursors

--- home-manager/hyprland/environment.nix
+++ home-manager/hyprland/environment.nix
@@ -6,15 +6,15 @@ let
 
   sessionVariables = config.desktop-flake.hyprland.additionalSessionVariables
     // optionalAttrs config.desktop-flake.nvidia {
-      # Nvidia: https://wiki.hyprland.org/Nvidia
-      LIBVA_DRIVER_NAME = "nvidia";
-      GBM_BACKEND = "nvidia-drm";
-      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-      WLR_NO_HARDWARE_CURSORS = 1;
-      NVD_BACKEND = "direct";
-      MOZ_DISABLE_RDD_SANDBOX = 1;
-    };
-in mkIf cfg.enable {
+    # Nvidia: https://wiki.hyprland.org/Nvidia
+    LIBVA_DRIVER_NAME = "nvidia";
+    GBM_BACKEND = "nvidia-drm";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    NVD_BACKEND = "direct";
+    MOZ_DISABLE_RDD_SANDBOX = 1;
+  };
+in
+mkIf cfg.enable {
   wayland.windowManager.hyprland.settings.env =
     mapAttrsToList (key: value: "${key}, ${toString value}") sessionVariables;
 }
